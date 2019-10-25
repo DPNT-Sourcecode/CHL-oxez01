1624bd75a18c10186e6ba25aa02877aa53d8541b
sbx_user1051 <sbx_user1051@169.254.229.93>
2019-10-25 10:53:22 +0000

    Fri Oct 25 10:53:22 UTC 2019

--- src/main/java/befaster/solutions/CHL/Item.java
+++ src/main/java/befaster/solutions/CHL/Item.java
@@ -1,5 +1,7 @@
 package befaster.solutions.CHL;
 
+import java.util.Objects;
+
 public final class Item {
     private final String sku;
     private final int price;
@@ -9,4 +11,22 @@ public final class Item {
         this.sku = sku;
         this.price = price;
     }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof Item)) {
+            return false;
+        }
+        Item o = (Item) other;
+        return Objects.equals(sku, o.sku) && Objects.equals(price, o.price);
+    }
 }
+

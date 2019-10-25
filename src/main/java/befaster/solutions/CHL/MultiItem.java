f1e3d610cc63d439696b8732689b5eb24d76262f
sbx_user1051 <sbx_user1051@169.254.229.93>
2019-10-25 15:02:10 +0000

    Fri Oct 25 15:02:10 UTC 2019

--- src/main/java/befaster/solutions/CHL/MultiItem.java
+++ src/main/java/befaster/solutions/CHL/MultiItem.java
@@ -1,42 +1,56 @@
 package befaster.solutions.CHL;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
 
 public class MultiItem {
 
     private final Item item;
-    private final int quantity;
-    private final int price;
+    private final List<Offer> offers;
 
-    public MultiItem(Item item, int quantity, int price) {
+    public MultiItem(Item item, List<Offer> offers) {
         this.item = item;
-        this.quantity = quantity;
-        this.price = price;
+        this.offers = offers;
     }
 
-    public Item getItem() {
-        return item;
+    public List<Offer> getOffers() {
+        return new LinkedList<>(offers);
     }
 
-    public int getQuantity() {
-        return quantity;
-    }
+    //    public Item getItem() {
+//        return item;
+//    }
 
-    public int getPrice() {
-        return price;
-    }
 
-    @Override
-    public boolean equals(Object other) {
-        if (!(other instanceof MultiItem)) {
-            return false;
+    static class Offer implements Comparable<Offer> {
+        private final int quantity;
+        private final int price;
+
+        Offer(int quantity, int price) {
+            this.quantity = quantity;
+            this.price = price;
         }
-        MultiItem o = (MultiItem) other;
-        return Objects.equals(item, o.item) && Objects.equals(quantity, o.quantity) && Objects.equals(price, o.price);
+
+        @Override
+        public boolean equals(Object other) {
+            if (!(other instanceof Offer)) {
+                return false;
+            }
+            Offer o = (Offer) other;
+            return Objects.equals(quantity, o.quantity) && Objects.equals(price, o.price);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(quantity, price);
+        }
+
+        @Override
+        public int compareTo(Offer o) {
+            return 0;
+        }
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(item, quantity, price);
-    }
 }
+

f545d3381d4c43922d3966a880d17b2ff22cf53e
sbx_user1051 <sbx_user1051@169.254.229.93>
2019-10-25 11:02:22 +0000

    Fri Oct 25 11:02:22 UTC 2019

--- src/test/java/befaster/solutions/CHL/ItemTest.java
+++ src/test/java/befaster/solutions/CHL/ItemTest.java
@@ -2,11 +2,26 @@ package befaster.solutions.CHL;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 public class ItemTest {
 
     @Test
-    void equalsAndHashCode() {
-        Item item1 = new Item("A")
-//        assertThat
+    public void equalsAndHashCodeSame() {
+        Item item1 = new Item("A", 50);
+        Item item2 = new Item("A", 50);
+        assertThat(item1.equals(item2), equalTo(true));
+        assertThat(item2.equals(item1), equalTo(true));
     }
+
+    @Test
+    public void equalsAndHashCodeDifferent() {
+        Item item1 = new Item("A", 50);
+        Item item2 = new Item("A", 60);
+        assertThat(item1.equals(item2), equalTo(true));
+        assertThat(item2.equals(item1), equalTo(true));
+    }
+
 }
+

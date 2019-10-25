6d4d4bf82c2ba1d8f3faa63adea59c0c0b708b68
sbx_user1051 <sbx_user1051@169.254.229.93>
2019-10-25 10:47:22 +0000

    Fri Oct 25 10:47:22 UTC 2019

---
+++ src/main/java/befaster/solutions/CHL/Item.java
@@ -0,0 +1,12 @@
+package befaster.solutions.CHL;
+
+public final class Item {
+    private final String sku;
+    private final int price;
+
+
+    public Item(String sku, int price) {
+        this.sku = sku;
+        this.price = price;
+    }
+}

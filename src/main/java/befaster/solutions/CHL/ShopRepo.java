8b551e1a8d9a73737ee211b40ba9166c6be9a558
sbx_user1051 <sbx_user1051@169.254.229.93>
2019-10-25 15:08:10 +0000

    Fri Oct 25 15:08:10 UTC 2019

--- src/main/java/befaster/solutions/CHL/ShopRepo.java
+++ src/main/java/befaster/solutions/CHL/ShopRepo.java
@@ -1,5 +1,7 @@
 package befaster.solutions.CHL;
 
+import com.google.common.collect.Lists;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,10 +10,14 @@ public class ShopRepo {
     static final Item ITEM_A = new Item("A", 50);
     static final Item ITEM_B = new Item("B", 30);
     static final Item ITEM_C = new Item("C", 20);
-    static final Item ITEM_D= new Item("D", 15);
+    static final Item ITEM_D = new Item("D", 15);
 
-    static final MultiItem MULTI_A = new MultiItem(ITEM_A, new MultiItem.Offer(3, 130));
-    static final MultiItem MULTI_B = new MultiItem(ITEM_B, 2, 45);
+    static final MultiItem.Offer offerA3 = new MultiItem.Offer(3, 130);
+    static final MultiItem.Offer offerA5 = new MultiItem.Offer(5, 200);
+    static final MultiItem MULTI_A = new MultiItem(ITEM_A, Lists.newArrayList(offerA3, offerA5));
+    //
+    static final MultiItem.Offer offerB2 = new MultiItem.Offer(2, 45);
+    static final MultiItem MULTI_B = new MultiItem(ITEM_B, Lists.newArrayList(offerB2));
     //
     static final Map<String, Item> items = new HashMap();
     static final Map<String, MultiItem> multiItems = new HashMap();
@@ -39,4 +45,5 @@ public class ShopRepo {
         return multiItems.containsKey(sku);
     }
 }
+
 

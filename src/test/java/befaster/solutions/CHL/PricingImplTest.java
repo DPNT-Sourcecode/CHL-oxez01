69cfbd57aba33ef66c75fda4fdbd5c026ce7e628
sbx_user1051 <sbx_user1051@169.254.229.93>
2019-10-25 15:47:10 +0000

    Fri Oct 25 15:47:10 UTC 2019

--- src/test/java/befaster/solutions/CHL/PricingImplTest.java
+++ src/test/java/befaster/solutions/CHL/PricingImplTest.java
@@ -34,7 +34,7 @@ public class PricingImplTest {
     public void evaluateItemOnMultiOfferManyTimes() {
         Pricing pricing = new PricingImpl(repo);
         BasketItem bi = new BasketItem(new Item("A", 50), 6);
-        assertThat(pricing.evaluate(bi), equalTo(260));
+        assertThat(pricing.evaluate(bi), equalTo(250));
     }
 
     @Test
@@ -46,3 +46,4 @@ public class PricingImplTest {
 
 
 }
+

33c6c010881fc72372275fc16db9c6ad484137d2
sbx_user1051 <sbx_user1051@169.254.229.93>
2019-10-25 13:47:22 +0000

    Fri Oct 25 13:47:22 UTC 2019

--- src/test/java/befaster/solutions/CHL/Scratch.java
+++ src/test/java/befaster/solutions/CHL/Scratch.java
@@ -2,19 +2,24 @@ package befaster.solutions.CHL;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Scratch {
 
     @Test
     public void testParsing() {
-        Map<String, Long> collect = "A".chars().mapToObj(c -> String.valueOf(c))
+        char[] chars = "BB".toCharArray();
+
+        Map<String, Long> collect = Stream.of(chars).map(c -> String.valueOf(c))
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
         System.out.println(collect);
 
     }
 }
+

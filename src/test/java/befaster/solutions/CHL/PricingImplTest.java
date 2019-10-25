393da0b1bedc9e1a40c765e4506f8a226d96cfc1
sbx_user1051 <sbx_user1051@169.254.229.93>
2019-10-25 12:26:22 +0000

    Fri Oct 25 12:26:22 UTC 2019

---
+++ src/test/java/befaster/solutions/CHL/PricingImplTest.java
@@ -0,0 +1,17 @@
+package befaster.solutions.CHL;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.*;
+
+public class PricingImplTest {
+
+    @Test
+    public void evaluateItemNotOnOffer() {
+        Pricing pricing = new PricingImpl();
+        BasketItem bi = new BasketItem(new Item("A", 10), 1);
+        assertThat(pricing.evaluate(bi), equalTo(10));
+    }
+
+}

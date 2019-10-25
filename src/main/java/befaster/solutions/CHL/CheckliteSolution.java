01e69efde77c88cdcc9aa67fda1a06cbd9adcc48
sbx_user1051 <sbx_user1051@169.254.229.93>
2019-10-25 12:20:22 +0000

    Fri Oct 25 12:20:22 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -12,14 +12,26 @@ import java.util.stream.Collectors;
 public class CheckliteSolution {
 
 
+    private final ShopRepo repo;
+    private final Pricing pricing;
+
+    public CheckliteSolution() {
+        repo = new ShopRepo();
+    }
 
     public Integer checklite(String skus) {
         boolean validInput = skus.chars().allMatch(c -> validSku(c));
         if (!validInput) {
             return -1;
         }
-//        skus.chars().mapToObj(c -> String.valueOf(c))
-//                .collect(Collectors.toMap(Function.identity(), Collectors.counting()));
+        Set<BasketItem> basket = skus.chars().mapToObj(c -> String.valueOf(c))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .map(e -> new BasketItem(repo.getItem(e.getKey()), e.getValue().intValue()))
+                .collect(Collectors.toSet());
+
+
+
 
         throw new SolutionNotImplementedException();
     }
@@ -34,3 +46,4 @@ public class CheckliteSolution {
     }
 
 }
+

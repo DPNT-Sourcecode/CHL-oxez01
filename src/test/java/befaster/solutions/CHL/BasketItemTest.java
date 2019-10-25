a1a3bed6445fbd558b6364ef272210b1fa1092b2
sbx_user1051 <sbx_user1051@169.254.229.93>
2019-10-25 11:17:22 +0000

    Fri Oct 25 11:17:22 UTC 2019

--- src/test/java/befaster/solutions/CHL/BasketItemTest.java
+++ src/test/java/befaster/solutions/CHL/BasketItemTest.java
@@ -9,9 +9,26 @@ public class BasketItemTest {
 
     @Test
     public void equalsAndHashCodeSame() {
-        Item item1 = new Item("A", 50);
-        Item item2 = new Item("A", 50);
+        BasketItem item1 = new BasketItem(new Item("A", 50), 2);
+        BasketItem item2 = new BasketItem(new Item("A", 50), 2);
         assertThat(item1.equals(item2), equalTo(true));
         assertThat(item2.equals(item1), equalTo(true));
     }
+
+    @Test
+    public void equalsAndHashCodeDifferentItem() {
+        BasketItem item1 = new BasketItem(new Item("B", 50), 2);
+        BasketItem item2 = new BasketItem(new Item("A", 50), 2);
+        assertThat(item1.equals(item2), equalTo(false));
+        assertThat(item2.equals(item1), equalTo(false));
+    }
+
+    @Test
+    public void equalsAndHashCodeDifferentQuantity() {
+        BasketItem item1 = new BasketItem(new Item("A", 50), 2);
+        BasketItem item2 = new BasketItem(new Item("A", 50), 4);
+        assertThat(item1.equals(item2), equalTo(false));
+        assertThat(item2.equals(item1), equalTo(false));
+    }
 }
+

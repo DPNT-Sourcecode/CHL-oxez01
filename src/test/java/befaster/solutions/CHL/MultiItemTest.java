82c912c0159f261289e50c5f5980cce2e42fcf9f
sbx_user1051 <sbx_user1051@169.254.70.189>
2019-10-29 17:59:33 +0000

    Tue Oct 29 17:59:33 UTC 2019

--- src/test/java/befaster/solutions/CHL/MultiItemTest.java
+++ src/test/java/befaster/solutions/CHL/MultiItemTest.java
@@ -1,5 +1,6 @@
 package befaster.solutions.CHL;
 
+import com.google.common.collect.Lists;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -12,6 +13,11 @@ public class MultiItemTest {
         ShopRepo repo = new ShopRepo();
         MultiItem a = repo.getMultiItem("A");
         assertThat(a.minimumMultiQuantity(), equalTo(3));
-
     }
+
+    static final MultiItem.Offer offerA3 = new MultiItem.Offer(3, 130);
+    static final MultiItem.Offer offerA5 = new MultiItem.Offer(5, 200);
+    static final MultiItem MULTI_A = new MultiItem(ITEM_A, Lists.newArrayList(offerA3, offerA5));
+
 }
+

1a709359a10060f71d5f7abcd0eaab9bd23ad29f
sbx_user1051 <sbx_user1051@169.254.70.189>
2019-10-29 18:59:33 +0000

    Tue Oct 29 18:59:33 UTC 2019

--- src/test/java/befaster/solutions/CHL/BogofItemTest.java
+++ src/test/java/befaster/solutions/CHL/BogofItemTest.java
@@ -1,6 +1,9 @@
 package befaster.solutions.CHL;
 
+import com.google.common.collect.Sets;
 import org.junit.Test;
+
+import java.util.Set;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -12,11 +15,29 @@ public class BogofItemTest {
     static final Item ITEM_B = new Item("A", 30);
 
     @Test
-    public void equalsAndHashCodeSame() {
-        BogofItem item1 = new BogofItem(ITEM_A, 2, ITEM_A, 1)
-        BogofItem item2 = new BogofItem(ITEM_A, 2, ITEM_A, 1)
-        assertThat(item1.equals(item2), equalTo(true));
-        assertThat(item2.equals(item1), equalTo(true));
+    public void equalsAndHashCodeFreebieSame() {
+        // for 2A 1 A free
+        BogofItem itemA1 = new BogofItem(ITEM_A, 2, ITEM_A, 1);
+        BogofItem itemA2 = new BogofItem(ITEM_A, 2, ITEM_A, 1);
+        assertThat(itemA1.equals(itemA2), equalTo(true));
+        assertThat(itemA2.equals(itemA1), equalTo(true));
     }
 
+    @Test
+    public void equalsAndHashCodeFreebieDifferent() {
+        // for 2A 1 A free
+        BogofItem itemB1 = new BogofItem(ITEM_A, 3, ITEM_B, 2);
+        BogofItem itemB2 = new BogofItem(ITEM_A, 3, ITEM_B, 2);
+        assertThat(itemB1.equals(itemB2), equalTo(true));
+        assertThat(itemB2.equals(itemB1), equalTo(true));
+    }
+
+    @Test
+    public void correctlyAdjustsBasket() {
+        BasketItem a1 = new BasketItem(ITEM_A, 3);
+        Set<BasketItem> basket = Sets.newHashSet(a1);
+        //
+        BogofItem itemA1 = new BogofItem(ITEM_A, 2, ITEM_A, 1);
+        
+    }
 }

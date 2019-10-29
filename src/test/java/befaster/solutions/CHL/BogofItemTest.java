022d92cd8282bc69b574b8ad8d03ea97298ed6cf
sbx_user1051 <sbx_user1051@169.254.70.189>
2019-10-29 18:56:33 +0000

    Tue Oct 29 18:56:33 UTC 2019

--- src/test/java/befaster/solutions/CHL/BogofItemTest.java
+++ src/test/java/befaster/solutions/CHL/BogofItemTest.java
@@ -8,11 +8,13 @@ import static org.junit.Assert.*;
 
 public class BogofItemTest {
 
+    static final Item ITEM_A = new Item("A", 50);
+    static final Item ITEM_B = new Item("A", 30);
 
     @Test
     public void equalsAndHashCodeSame() {
-        Item item1 = new Item("A", 50);
-        Item item2 = new Item("A", 50);
+        BogofItem item1 = new BogofItem(ITEM_A, 2, ITEM_A, 1)
+        BogofItem item2 = new BogofItem(ITEM_A, 2, ITEM_A, 1)
         assertThat(item1.equals(item2), equalTo(true));
         assertThat(item2.equals(item1), equalTo(true));
     }
